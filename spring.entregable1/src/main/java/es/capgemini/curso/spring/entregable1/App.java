f3e5be107b9aa7c20795a6fcee9d45d3c6448bc5
Victor Esteban <victor.esteban@capgemini.com>
2021-01-31 17:00:37 +0100

    Creacion de proyecto y POM padre

---
+++ spring.entregable1/src/main/java/es/capgemini/curso/spring/entregable1/App.java
@@ -0,0 +1,13 @@
+package es.capgemini.curso.spring.entregable1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
